07f9d5cb82d32a213f724694c79a193cb4c22b2c
Martin Sandström <martin@marteinn.se>
2015-12-03 22:21:49 +0100

    Added db health-check

--- Django-GeoDjango-Postgres/docker-entrypoint.sh
+++ Django-GeoDjango-Postgres/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+until nc -z db 5432; do
+    echo "$(date) - waiting for postgres..."
+    sleep 1
+done
+
 echo Running migrations
 cd /app && python manage.py migrate                  # Apply database migrations
 # cd /src && python manage.py collectstatic --noinput  # Collect static files
