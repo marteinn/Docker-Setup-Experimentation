82b9376c9d39e1d6313b7be9b18f77e149176210
Martin Sandström <martin@marteinn.se>
2016-02-14 17:02:08 +0100

    Added django env-autoload to manage

--- 10-Docker-Composer-Ansible-Fabric-ECR-2.0/src/manage.py
+++ 10-Docker-Composer-Ansible-Fabric-ECR-2.0/src/manage.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 import sys
+import inspect
+import dotenv
+
+
+# Fetches all environment variables from your .env file
+try:
+    inspect_file = inspect.getfile(inspect.currentframe())
+    env_path = os.path.dirname(os.path.abspath(inspect_file))
+    dotenv.load_dotenv("%s/.env" % (env_path,))
+except Exception, e:
+    pass
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
