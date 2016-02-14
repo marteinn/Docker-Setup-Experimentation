2c4129ec40651c7693c82b6b48278b29c75ceb18
Martin Sandström <martin@marteinn.se>
2016-02-14 17:01:39 +0100

    Renamed env-vars to avoid colliding with composer ones

---
+++ 10-Docker-Composer-Ansible-Fabric-ECR-2.0/src/core/settings/prod.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from base import *  # NOQA
+
+
+# Enable caching of templates in production environment
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+)
